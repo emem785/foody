145d3114a2f2a10867077d6d47a6f4464a199565
emem785 <32265358+emem785@users.noreply.github.com>
2021-07-08 12:53:19 +0100

    first commit

---
+++ lib/ui/main_menu_page/widget/cart_icon.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:fooody/change_notifiers/cart_item_change_notifier.dart';
+import 'package:provider/provider.dart';
+
+class CartIcon extends StatelessWidget {
+  const CartIcon({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _itemChangeNotifier = Provider.of<CartItemChangeNotifier>(context);
+    return Padding(
+      padding: const EdgeInsets.only(top: 6),
+      child: Stack(children: [
+        Icon(Icons.shopping_cart_outlined, size: 30, color: Colors.black),
+        _itemChangeNotifier.noOfItems == 0
+            ? SizedBox()
+            : Positioned(
+                left: 13,
+                child: ClipOval(
+                  child: Container(
+                    color: Theme.of(context).primaryColor,
+                    height: 14,
+                    width: 14,
+                    child: Center(
+                      child: Text(_itemChangeNotifier.noOfItems.toString(),
+                          style: TextStyle(color: Colors.white, fontSize: 11)),
+                    ),
+                  ),
+                ),
+              )
+      ]),
+    );
+  }
+}
